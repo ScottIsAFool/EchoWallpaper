e98a0e7b45f19dee575efc7dbd9f56a1df2c92de
ScottIsAFool <scottisafool@live.co.uk>
2014-11-26 22:18:26 +0000

    Added backgroundtaskservice

---
+++ EchoWallpaper.WindowsPhone.Silverlight/Services/BackgroundTaskService.cs
@@ -0,0 +1,8 @@
+﻿namespace EchoWallpaper.WindowsPhone.Silverlight.Services
+{
+    public class BackgroundTaskService
+    {
+        private static BackgroundTaskService _current;
+        public static BackgroundTaskService Current { get { return _current ?? (_current = new BackgroundTaskService()); } }
+    }
+}
