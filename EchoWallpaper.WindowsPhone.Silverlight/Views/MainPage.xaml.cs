dbf41a1624d7e6d9ac99d5242bf556f18485722b
ScottIsAFool <scottisafool@live.co.uk>
2014-11-30 11:14:33 +0000

    Fixed app bar on back navigation, sorted name too

--- EchoWallpaper.WindowsPhone.Silverlight/Views/MainPage.xaml.cs
+++ EchoWallpaper.WindowsPhone.Silverlight/Views/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace EchoWallpaper.WindowsPhone.Silverlight.Views
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Navigation;
+
+namespace EchoWallpaper.WindowsPhone.Silverlight.Views
 {
     public partial class MainPage
     {
@@ -6,6 +10,17 @@
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (ApplicationBar != null)
+            {
+                ApplicationBar.BackgroundColor = (Color) Application.Current.Resources["AppBarBackground"];
+                ApplicationBar.ForegroundColor = (Color) Application.Current.Resources["PhoneAccentColor"];
+            }
+
+            base.OnNavigatedTo(e);
         }
     }
 }
