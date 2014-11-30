a2ee304737a88bbd0125eb8e3f88563594941666
ScottIsAFool <scottisafool@live.co.uk>
2014-11-30 10:08:41 +0000

    Win8.1 implementation projects.

---
+++ EchoWallpaper.Windows.Background/BackgroundTask.cs
@@ -0,0 +1,15 @@
+﻿using EchoWallpaper.Windows.Shared;
+using Windows.ApplicationModel.Background;
+
+namespace EchoWallpaper.Windows.Background
+{
+    public sealed class BackgroundTask : IBackgroundTask
+    {
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var settings = new AppSettings()
+            var lockScreen = new LockScreenService();
+            
+        }
+    }
+}
---
+++ EchoWallpaper.Windows.Shared/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using EchoWallpaper.Core.Interfaces;
+
+namespace EchoWallpaper.Windows.Shared
+{
+    public class AppSettings : IAppSettings
+    {
+        public bool DownloadImageForStartScreen { get; set; }
+        public bool AutomaticallyUpdateLockScreen { get; set; }
+        public void Save()
+        {
+        }
+    }
+}
