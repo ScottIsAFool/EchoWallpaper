c2a4c6a9c88f58842f047a1749d93a9d8aae6947
Scott Lovegrove <scottisafool@live.co.uk>
2015-08-01 17:34:06 +0100

    Desktop UI done

--- EchoWallpaper.Universal/Views/MainPage.xaml.cs
+++ EchoWallpaper.Universal/Views/MainPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using Windows.Foundation.Metadata;
+﻿using Windows.ApplicationModel.Core;
+using Windows.Foundation.Metadata;
 using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace EchoWallpaper.Universal.Views
 {
@@ -17,6 +20,16 @@ namespace EchoWallpaper.Universal.Views
             {
                 StatusBar.GetForCurrentView().HideAsync();
             }
+
+            var titleBar = CoreApplication.GetCurrentView().TitleBar;
+            titleBar.ExtendViewIntoTitleBar = true;
+
+            var accentBrush = Application.Current.Resources["AccentBrush"] as SolidColorBrush;
+            var altAccentBrush = Application.Current.Resources["AltAccentBrush"] as SolidColorBrush;
+
+            var titleBarWithButtons = ApplicationView.GetForCurrentView().TitleBar;
+            titleBarWithButtons.ButtonBackgroundColor = accentBrush?.Color;
+            titleBarWithButtons.ButtonForegroundColor = altAccentBrush?.Color;
         }
     }
 }
